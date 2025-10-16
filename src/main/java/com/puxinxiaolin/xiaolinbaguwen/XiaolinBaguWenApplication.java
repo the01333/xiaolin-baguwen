c53b36c5f4101e00eabb376be8ff88d95eb53aae
the01333 <3149696140@qq.com>
2025-10-16 21:11:17 +0800

    Initial commit

---
+++ src/main/java/com/puxinxiaolin/xiaolinbaguwen/XiaolinBaguWenApplication.java
@@ -0,0 +1,13 @@
+package com.puxinxiaolin.xiaolinbaguwen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaolinBaguWenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaolinBaguWenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/puxinxiaolin/xiaolinbaguwen/XiaolinBaguWenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puxinxiaolin.xiaolinbaguwen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaolinBaguWenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
