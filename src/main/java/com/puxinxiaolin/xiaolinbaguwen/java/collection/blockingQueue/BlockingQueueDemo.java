e0786a25f3247d6c0249e0aa2f1d17721e9039e4
the01333 <3149696140@qq.com>
2025-10-18 15:44:44 +0800

    feat: Java集合八股暂时更新完

---
+++ src/main/java/com/puxinxiaolin/xiaolinbaguwen/java/collection/blockingQueue/BlockingQueueDemo.java
@@ -0,0 +1,35 @@
+package com.puxinxiaolin.xiaolinbaguwen.java.collection.blockingQueue;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class BlockingQueueDemo {
+    public static void main(String[] args) {
+        BlockingQueue<Integer> queue = new LinkedBlockingQueue<>();
+        
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Producing: " + i);
+                    queue.put(i);
+                    TimeUnit.SECONDS.sleep(1);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }, "productor").start();
+        
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    Integer value = queue.take();
+                    System.out.println("Consuming: " + value);
+                    TimeUnit.SECONDS.sleep(2);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }, "consumer").start();
+    }
+}
